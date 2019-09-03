c45abfbd669809d1349ae2a32db6f68357cf758c
Daniel <vilson.daniel@hotmail.com>
2019-09-03 10:55:04 -0300

    pi

---
+++ app/src/main/java/com/example/daniel/pifazendo/ControlerArtigo.java
@@ -0,0 +1,4 @@
+package com.example.daniel.pifazendo;
+
+public class ControlerArtigo {
+}
---
+++ app/src/main/java/com/example/daniel/pifazendo/artigo.java
@@ -0,0 +1,42 @@
+package com.example.daniel.pifazendo;
+
+public class artigo {
+    private String titulo;
+    private String categoria;
+    private String descricao;
+    private String publico;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPublico() {
+        return publico;
+    }
+
+    public void setPublico(String publico) {
+        this.publico = publico;
+    }
+
+
+}
